76c1d82914904bfb461884877fe9592c02271a73
cristian20252025 <cmperez059@gmail.com>
2025-04-03 08:59:02 -0500

    Se agregaron las funciones para editar, agregar y eliminar servicios

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,4 +1,21 @@
+import json
+import os
 
+
+archivo_json = 'servicios_fotograficos.json'
+
+
+def cargar_servicios():
+    if os.path.exists(archivo_json):
+        with open(archivo_json, 'r') as archivo:
+            return json.load(archivo)
+    else:
+        return []
+
+
+def guardar_servicios(servicios):
+    with open(archivo_json, 'w') as archivo:
+        json.dump(servicios, archivo, indent=4)
 
 
 
