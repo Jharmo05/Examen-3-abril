847b2d3f4ebc6c61090a7a714a3adf925279ee84
cristian20252025 <cmperez059@gmail.com>
2025-04-03 08:46:02 -0500

    se agrego las opciones del menu

---
+++ main.py
@@ -0,0 +1,33 @@
+def menu():
+    while True:
+        print("\n--- Menú de Gestión de Servicios Fotográficos ---")
+        print("1. Agregar servicio")
+        print("2. Editar servicio")
+        print("3. Eliminar servicio")
+        print("4. Ver todos los servicios")
+        print("5. Salir")
+
+        opcion = input("Seleccione una opción (1-5): ")
+
+        if opcion == '1':
+            agregar_servicio()
+        elif opcion == '2':
+            editar_servicio()
+        elif opcion == '3':
+            eliminar_servicio()
+        elif opcion == '4':
+            servicios = cargar_servicios()
+            if servicios:
+                print("\nServicios Fotográficos Registrados:")
+                for servicio in servicios:
+                    print(servicio)
+            else:
+                print("No hay servicios registrados.")
+        elif opcion == '5':
+            print("Saliendo...")
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
+
+menu()
+MENU
